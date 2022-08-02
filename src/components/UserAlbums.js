8aabde38fa2d5fa988a54c894c5dd4fd4c11c63b
Nadezhda Georgieva <32280335+altcoda@users.noreply.github.com>
2022-08-02 19:31:24 +0300

    minor changes

--- src/components/UserAlbums.js
+++ src/components/UserAlbums.js
@@ -19,12 +19,10 @@ export const UserAlbums = () => {
 
     return(
         <div className="container page">
-            <div className="row">
-                {userAlbums ? userAlbums.map(album => <UserAlbum key={album.objectId} album={album} />) :
-                    <div>
-                        <h1>No albums. Be the first to add your album from <Link to="/albums/add">here!</Link></h1>
-                    </div>}
-            </div>
+            {userAlbums ? userAlbums.map(album => <UserAlbum key={album.objectId} album={album} />) :
+                <div>
+                    <h1>No albums. Be the first to add your album from <Link to="/albums/add">here!</Link></h1>
+                </div>}
         </div>
     )
 }
--- src/pages/EditAlbumPage.js
+++ src/pages/EditAlbumPage.js
@@ -5,7 +5,6 @@ import { AlbumForm } from '../components/AlbumForm'
 export const EditAlbumPage = () => {
     const {pathname} = useLocation();
     const albumId = pathname.split('/').pop();
-    console.log(albumId)
 
     return(
         <div id="edit-album" className="edit-album container page">
